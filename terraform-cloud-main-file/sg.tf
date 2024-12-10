e79a8a9b0a09440beb23b2d92d9c9d9d7247ea6a
E Njang <georgegedeon2012@gmail.com>
2024-12-10 10:03:40 -0500

    Add files via upload

---
+++ terraform-cloud-main-file/sg.tf
@@ -0,0 +1,8 @@
+module "dev_sg_1" {
+  source  = "app.terraform.io/saikiranpi_devsecops/sg/modules"
+  version = "1.0.0"
+  vpc_name      = module.dev_vpc_1.vpc_name
+  vpc_id        = module.dev_vpc_1.vpc_id
+  environment   = module.dev_vpc_1.environment
+  service_ports = ["80", "443", "445", "8080", "22", "3389"]
+}
